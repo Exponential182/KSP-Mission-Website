70277bf5a50bc4d7103e927e31a49c01ea9c8c6d
Your Name~ <mthomas4773@gmail.com>
2025-04-09 13:37:35 +1200

    Added 404 Page and Made base layout link properly

--- app.py
+++ app.py
@@ -6,7 +6,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("layout.hmtml")
+    return render_template("layout.html")
+
+
+@app.errorhandler(404)
+def page_not_found_error(error):
+    return render_template("404.html"), 404
+
 
 if __name__ == '__main__':
     app.run(debug=True)
