798e8dc7dacd12191f71af1a4e72454daad74b25
Exponential182 <mthomas4773@gmail.com>
2025-07-28 09:36:27 +1200

    Added code comments, and 500 error handler and error page.

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
-from flask import Flask, render_template, request
 import sqlite3
 from os import path
 
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
+# Generate the absolute path to the database to prevent the path leading 
+# to the wrong database or creating an empty one and finding nothing.
 runtime_directory = path.abspath(path.dirname(__file__))
 db_path = path.join(runtime_directory, "ksp.db")
 
 
 def lookup_query(query: str):
-    """ A function to simplify the execution of pre-sanitised queries. """
+    """ A function to simplify the execution of pre-sanitised and 
+    generated queries. 
+    """
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
     cursor.execute(query)
@@ -20,6 +25,8 @@ def mission_data_formatter(data_row: list):
     """ A Function to adjust the formatting of the data from the mission query 
     to a useful format.
     """
+    # References are based on the databse structure so need to be updated if 
+    # more columns are added to the database
     columns = [
         False, False, "Mission Goal", "Crew Count",
         "Estimated Mission Duration", "Power Source", "Axial Control System",
@@ -29,29 +36,32 @@ def mission_data_formatter(data_row: list):
         "Argument of Periapsis (deg)", "Latitude of Landing",
         "Longitude of Landing"
     ]
-    m_to_km = 0.001
+    meters_to_km = 0.001
     multiplicative_transforms = {
-        11: m_to_km, 12: m_to_km, 13: m_to_km, 14: seconds_to_time,
+        11: meters_to_km, 12: meters_to_km, 13: meters_to_km, 
+        14: seconds_to_time
     }
     for key, value in multiplicative_transforms.items():
-        if callable(value) is True and data_row[key]:
+        if callable(value) and data_row[key]: #Checks for a function reference.
             data_row[key] = value(data_row[key])
-        elif type(value) == float and type(data_row[key]) == (float or int):
+        elif isinstance(value, float) and isinstance(data_row[key], (int, float)):
             data_row[key] = round(data_row[key] * value, 3)
     formatted_datarow = list(zip(columns, data_row))
     return formatted_datarow
 
 
 def seconds_to_time(seconds: float):
+    """ A function to convert the any values of time in seconds into a
+    ydhms (years, days, hours, minutes, seconds) format.
+    """
     time_in_seconds = {31536000: "y", 86400: "d", 3600: "h", 60: "m"}
     output_time_string = ""
-    for mult, time_string in time_in_seconds.items():
-        print(seconds)
-        if seconds // mult == 0:
+    for multiplier, time_string in time_in_seconds.items():
+        if seconds // multiplier == 0:
             continue
         else:
-            num_of_time_intervals = seconds // mult
-            seconds -= num_of_time_intervals * mult
+            num_of_time_intervals = seconds // multiplier
+            seconds -= num_of_time_intervals * multiplier
             output_time_string += f"{int(num_of_time_intervals)}{time_string} "
     if seconds > 0:
         output_time_string += f"{seconds:.2f}s"
@@ -78,12 +88,9 @@ def missions():
 
 @app.route("/mission/<int:mission_id>")
 def mission(mission_id: int):
-    orbital_parameter_and_landing_columns = [
-        "Semi Major Axis (km)", 
-        "Periapsis (km)", "Apoapsis (km)", "Orbital Period", "Eccentricity", 
-        "Inclination", "Longitude of the Ascending Node",
-        "Argument of Periapsis", "Latitude of Landing", "Longitude of Landing"
-    ]
+    """ A Function to gather and format the data on a mission then render the 
+    HTML template to display the mission data to the end user.
+    """
     query = f"SELECT * FROM Mission WHERE id = {mission_id}"
     results = lookup_query(query)
     if len(results) > 0:
@@ -117,6 +124,7 @@ def stages():
     query = """SELECT name, length, top_diameter, bottom_diameter, 
             image_reference, id FROM Stage ORDER BY name ASC"""
     results = lookup_query(query)
+    # Format the data to avoid computational logic in Jinja/HTML template.
     results = [(a[0], a[1], round((a[2] + a[3])/2, 2), a[4], a[5]) for a in results]
     return render_template("stages.html", title="KSP Mission Library",
                            data=results)
@@ -136,17 +144,23 @@ def glossary():
 
 @app.route("/sitemap")
 def sitemap():
-    """ A function to render the sitemap, links to every
-    non-paramertried query. 
+    """ A function to render the sitemap, which contains lnks to every page not
+    defined by a procedurally generated query.
     """
     return render_template("sitemap.html", title="KSP Mission Library")
 
 
 @app.errorhandler(404)
 def page_not_found_error(error):
-    """ A Function to render the error page whena 404 error happens."""
+    """ A function to render the 404 page when an invalid link is caused."""
     url = request.url
     return render_template("404.html", message  = "The url does not exist.", url = url), 404
+
+
+@app.errorhandler(500)
+def internal_servef_error(error):
+    """A function to render a page when there is a server side error."""
+    return render_template("500.html")
 
 
 if __name__ == '__main__':
